22cd6fafd566ee23a4d2b86bf57f66fd90b30a60
Emily Sellers <emilymelindasellers@gmail.com>
2022-09-21 13:04:27 -0700

    load event, displayContinentOptions

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* Imports */
 // > Part A: Import `getCountries` from fetch-utils.js
-import { getCountries } from './fetch-utils.js';
+import { getContinents, getCountries } from './fetch-utils.js';
 // > Part B: Import `getContinents` from fetch-utils.js
 import { renderContinentOption, renderCountry } from './render-utils.js';
 
@@ -21,10 +21,12 @@ window.addEventListener('load', async () => {
     // > Part A: call findCountries (with no arguments)
     findCountries();
     // > Part B: await the call to get continents to get the response
-
+    const response = await getContinents();
     // > Part B: Assign to state the:
     //      - error,
     //      - data (to the continents variable)
+    error = response.error;
+    continents = response.data;
 
     if (!error) {
         displayContinentOptions();
@@ -84,5 +86,7 @@ function displayNotifications() {
 function displayContinentOptions() {
     for (const continent of continents) {
         // > Part B: render and append options to select
+        const option = renderContinentOption(continent);
+        continentSelect.append(option);
     }
 }
